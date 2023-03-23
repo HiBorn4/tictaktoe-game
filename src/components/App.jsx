2757e26abd6d95ea8a748bf48f98bab1d85dc15d
HiBorn4 <hiborn4@gmail.com>
2023-03-24 00:06:57 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as contact from './contacts';
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+        </div>
+        <div className="bottom">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/contacts.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Beyonce () {
+	return <div>
+		<h2>Beyonce</h2>
+		<img src = "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" alt = "Beyonce's Image" />
+		<p>+123 456 789</p>
+		<p>b@beyonce.com</p>
+	</div>
+}
+
+function Jack_Bauer () {
+	return <div>
+		<h2>Jack Bauer</h2>
+		<img src = "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" alt = "Jack Bauer's Image" />
+		<p>+987 654 321</p>
+		<p>jack@nowhere.com</p>
+	</div>
+}
+
+function Chuck_Norris () {
+	return <div>
+		<h2>Chuck Norris</h2>
+		<img src = "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" alt = "Chuck Norris's Image" />
+		<p>+918 372 574</p>
+		<p>gmail@chucknorris.com</p>
+	</div>
+}
+
+export default Beyonce_Top
+export {Beyonce_Bottom, Jack_Bauer_Bottom, Jack_Bauer_Top, Chuck_Norris_Bottom, Chuck_Norris_Top}
